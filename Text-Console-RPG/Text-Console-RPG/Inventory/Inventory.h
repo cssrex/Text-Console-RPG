1c32a878924c5b0bbb210b103c703c50118ec7cd
cssrex <cssrex2002@gmail.com>
2026-07-31 11:34:23 +0900

    fix : file move

--- Text-Console-RPG/Text-Console-RPG/Inventory/Inventory.h
+++ Text-Console-RPG/Text-Console-RPG/Inventory/Inventory.h
@@ -1,8 +1,10 @@
 ﻿#pragma once
 
+#include <iostream>
 #include <vector>
-#include <map>
-#include <memory>
+#include <string>
+#include <algorithm>
+#include "Enums.h"
 #include "Item.h"
 
 using namespace std;
@@ -11,21 +13,19 @@ class Player;
 
 class Inventory {
 private:
-	// 인벤토리 소유
-	vector<unique_ptr<Item>> inventory_;
-
-	// 참조 장비 슬롯
-	map<EquipmentSlot, EquipmentItem*> equipment_;
+	vector<Item> inventory_;
+	vector<Item> equipment_;
 
 public:
 	// 아이템 획득
-	void AddItem(unique_ptr<Item> item);
+	void AddItem(const Item& item, int count);
 
 	// 인벤토리 메뉴
 	void InventoryMenu(Player& player);
 
 	// 아이템 선택
-	int SelectEquipment(EquipmentType type);
+	int SelectWeapon();
+	int SelectArmor();
 	int SelectConsumable();
 	int SelectLoot();
 
@@ -40,14 +40,19 @@ public:
 	// 장비 착용
 	void WearEquipment(Player& player, int index);
 	// 장비 해제
-	void TakeOffEquipment(Player& player, EquipmentSlot slot);
+	void TakeOffEquipment(Player& player, int index);
+
 	// 착용 중인 장비인지 검사
-	bool IsEquipped(const EquipmentItem& item);
+	bool IsEquipped(const Item& item);
 
 	// 인벤토리에서 제거
-	bool RemoveItem(int index, int count);
+	void RemoveItem(int index, int count);
 
-	const vector<unique_ptr<Item>>& GetInventory() const { return inventory_; }
+	const vector<Item>& GetInventory() const {
+		return inventory_;
+	}
 
-	const map<EquipmentSlot, EquipmentItem*>& GetEquipment() const { return equipment_; }
+	const vector<Item>& GetEquipment() const {
+		return equipment_;
+	}
 };
