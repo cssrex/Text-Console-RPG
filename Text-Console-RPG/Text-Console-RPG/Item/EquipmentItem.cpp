7051275f07fbd7b13e5c44f0223841f55c431608
comugong <hieto4837@gmail.com>
2026-08-03 21:03:45 +0900

    Update EquipmentItem.cpp
    UI 수정
    장비 착용/해제 시 능력치 변화 출력

--- Text-Console-RPG/Text-Console-RPG/Item/EquipmentItem.cpp
+++ Text-Console-RPG/Text-Console-RPG/Item/EquipmentItem.cpp
@@ -5,62 +5,117 @@
 
 using namespace std;
 
-void EquipmentItem::Equip(Player& player) {
-	auto PrintMessage = [](const string& message) {
-			const int BOX_WIDTH = 117;
+void EquipmentItem::Equip(Player& player)
+{
+    auto PrintMessage = [](const string& message)
+        {
+            const int BOX_WIDTH = 117;
 
-			cout << "| " << message;
-			int space =	BOX_WIDTH - LogManager::GetInstance().GetDisplayWidth(message);
+            cout << "| " << message;
 
-			for (int i = 0; i < space; i++)	cout << " ";
-			cout << "|\n";
-		};
+            int space =
+                BOX_WIDTH -
+                LogManager::GetInstance().GetDisplayWidth(message);
 
-	switch (equipmentType_) {
-	case EquipmentType::Weapon:
-		player.SetAttack(player.GetAttack() + attackValue_);
-		break;
+            for (int i = 0; i < space; i++)
+                cout << " ";
 
-	case EquipmentType::Armor:
-		player.SetDefense(player.GetDefense() + defenseValue_);
-		player.SetMaxHp(player.GetMaxHp() + healthValue_);
-		break;
+            cout << "|\n";
+        };
 
-	default:
-		PrintMessage("잘못된 장비 타입입니다.");
-		break;
-	}
+    PrintMessage(name_ + "을(를) 착용했습니다.");
+
+    switch (equipmentType_)
+    {
+    case EquipmentType::Weapon:
+
+        player.SetAttack(player.GetAttack() + attackValue_);
+
+        PrintMessage("공격력 +" + to_string(attackValue_));
+
+        break;
+
+    case EquipmentType::Armor:
+
+        player.SetDefense(player.GetDefense() + defenseValue_);
+        player.SetMaxHp(player.GetMaxHp() + healthValue_);
+
+        if (defenseValue_ > 0)
+        {
+            PrintMessage("방어력 +" + to_string(defenseValue_));
+        }
+
+        if (healthValue_ > 0)
+        {
+            PrintMessage("최대 HP +" + to_string(healthValue_));
+        }
+
+        break;
+
+    default:
+
+        PrintMessage("잘못된 장비 타입입니다.");
+        break;
+    }
 }
 
-void EquipmentItem::TakeOff(Player& player) {
-	auto PrintMessage = [](const string& message) {
-			const int BOX_WIDTH = 117;
+void EquipmentItem::TakeOff(Player& player)
+{
+    auto PrintMessage = [](const string& message)
+        {
+            const int BOX_WIDTH = 117;
 
-			cout << "| " << message;
-			int space =	BOX_WIDTH - LogManager::GetInstance().GetDisplayWidth(message);
+            cout << "| " << message;
 
-			for (int i = 0; i < space; i++) cout << " ";
-			cout << "|\n";
-		};
+            int space =
+                BOX_WIDTH -
+                LogManager::GetInstance().GetDisplayWidth(message);
 
-	switch (equipmentType_) {
-	case EquipmentType::Weapon:
-		player.SetAttack(player.GetAttack() - attackValue_);
-		break;
+            for (int i = 0; i < space; i++)
+                cout << " ";
 
-	case EquipmentType::Armor:
-		player.SetDefense(player.GetDefense() - defenseValue_);
-		player.SetMaxHp(player.GetMaxHp() - healthValue_);
+            cout << "|\n";
+        };
 
-		if (player.GetHp() > player.GetMaxHp()) {
-			player.SetHp(player.GetMaxHp());
-		}
-		break;
+    PrintMessage(name_ + "을(를) 해제했습니다.");
 
-	default:
-		PrintMessage("잘못된 장비 타입입니다.");
-		break;
-	}
+    switch (equipmentType_)
+    {
+    case EquipmentType::Weapon:
+
+        player.SetAttack(player.GetAttack() - attackValue_);
+
+        PrintMessage("공격력 -" + to_string(attackValue_));
+
+        break;
+
+    case EquipmentType::Armor:
+
+        player.SetDefense(player.GetDefense() - defenseValue_);
+        player.SetMaxHp(player.GetMaxHp() - healthValue_);
+
+        if (player.GetHp() > player.GetMaxHp())
+        {
+            player.SetHp(player.GetMaxHp());
+        }
+
+        if (defenseValue_ > 0)
+        {
+            PrintMessage("방어력 -" + to_string(defenseValue_));
+        }
+
+        if (healthValue_ > 0)
+        {
+            PrintMessage("최대 HP -" + to_string(healthValue_));
+        }
+
+        break;
+
+    default:
+
+        PrintMessage("잘못된 장비 타입입니다.");
+        break;
+    }
 }
 
 void EquipmentItem::Enhance() {
