40212ed1fbe40a8be2dbab97736e48faed1b22ea
comugong <hieto4837@gmail.com>
2026-08-04 14:17:00 +0900

    Update Store
    상점 중간 수정본
    루프 오류 수정

--- Text-Console-RPG/Text-Console-RPG/Store/Store.h
+++ Text-Console-RPG/Text-Console-RPG/Store/Store.h
@@ -14,64 +14,41 @@ protected:
     // 판매 아이템 목록
     vector<unique_ptr<Item>> items_;
 
-
     // 결과 메시지 출력
-    void PrintMessageBox(
-        const string& ascii,
-        const string& title,
-        const string& message
-    );
-
+    void PrintMessageBox(const string& ascii,const string& title,const string& message);
 
 public:
     virtual ~Store() = default;
 
-
     // 상점 메뉴
     virtual void StoreMenu(Player& player, Inventory& inventory) = 0;
-
 
     // 구매
     BuyResult BuyItem(Player& player, Inventory& inventory, int index);
 
-
     // 판매
     bool SellItem(Player& player, Inventory& inventory, int index);
 
-
     // 판매 메뉴
     void SellMenu(Player& player, Inventory& inventory, const string& ascii);
-
 
     // 아이템 출력
     void ShowItems() const;
 };
 
-
-
 class PotionStore : public Store {
-
 public:
-
     PotionStore();
-
 
     void StoreMenu(Player& player, Inventory& inventory) override;
 
 };
 
-
-
 class Blacksmith : public Store {
-
 public:
-
     Blacksmith();
 
-
     void StoreMenu(Player& player, Inventory& inventory) override;
-
-
     void Enhance(Player& player, Inventory& inventory);
 
 };
